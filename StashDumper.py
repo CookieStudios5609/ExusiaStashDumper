ed77396492c426fc57e5deaac3bcc2e4b7af3045
2600 <78653311+CookieStudios5609@users.noreply.github.com>
2021-04-23 21:08:42 -0400

    Feat: Add  event
    This commit adds an  event that prints basic info to the console and sets the bot's activity to a help command

--- StashDumper.py
+++ StashDumper.py
@@ -11,4 +11,10 @@ bot = commands.Bot(command_prefix=prefix, intents=intents)
 bot.load_extension('meme')
 
 
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has successfully connected to Discord!\nPing: {round(bot.latency * 1000, 2)}ms. My prefix is {prefix}')
+    activity_type = discord.ActivityType.playing
+    await bot.change_presence(activity=discord.Activity(type=activity_type, name=f"{prefix}help or {prefix}about"))
+
 bot.run(os.getenv("TOKEN"))
